87b06f5c95d58341cd3893dd220c5bdd0457a954
Facundo Berges <facundo.h.berges@gmail.com>
2024-10-27 18:52:26 -0300

    ordenado y paginacion de productos, botones y eventos correspondientes

--- js/products.js
+++ js/products.js
@@ -1,22 +1,138 @@
 import { searchBarListener } from './index.js';
-import { getProducts } from './products/get-products.js';
+import { getAllProducts, paginateProducts } from './products/get-products.js';
 import { renderProducts } from './products/render-products.js';
+import { orderProducts } from './products/order-products.js';
+
+const PRODUCTS_PER_PAGE = 20;
+let currentPage = 1;
+let from = 1;
+let to = PRODUCTS_PER_PAGE;
+let totalPages = 1;
 
 let productsArray = [];
+let orderedProducts = [];
+let paginatedProducts = [];
+
+/**
+ * Updates 'from' and 'to' variables.
+ *
+ * @param {number} currentPage - The current page.
+ */
+const updateFromTo = (currentPage) => {
+	from = currentPage * PRODUCTS_PER_PAGE - PRODUCTS_PER_PAGE + 1;
+	to = currentPage * PRODUCTS_PER_PAGE;
+};
+
+/**
+ * Renders the current page.
+ */
+const renderCurrentPage = () => {
+	const currentPageElement = document.querySelector('#current-page');
+	currentPageElement.textContent = currentPage;
+};
+
+/**
+ * Updates the state of the pagination buttons.
+ *
+ * @param {number} totalPages - The total number of pages.
+ */
+const updatePaginationButtonsState = (totalPages) => {
+	const firstPage = document.querySelector('#first-page');
+	const previousPage = document.querySelector('#previous-page');
+	const nextPage = document.querySelector('#next-page');
+	const lastPage = document.querySelector('#last-page');
+
+	if (currentPage === 1) {
+		firstPage.setAttribute('disabled', true);
+		previousPage.setAttribute('disabled', true);
+	} else {
+		firstPage.removeAttribute('disabled');
+		previousPage.removeAttribute('disabled');
+	}
+
+	if (currentPage === totalPages) {
+		nextPage.setAttribute('disabled', true);
+		lastPage.setAttribute('disabled', true);
+	} else {
+		nextPage.removeAttribute('disabled');
+		lastPage.removeAttribute('disabled');
+	}
+};
+
+/**
+ * Handles the pagination buttons.
+ *
+ * @param {PointerEvent} event - The event object.
+ */
+const handlePaginationButtons = async (event) => {
+	const action = event.target.value;
+
+	if (currentPage === 1 && (action === 'previous' || action === 'first')) return;
+	if (currentPage === totalPages && (action === 'next' || action === 'last')) return;
+
+	switch (action) {
+		case 'previous':
+			currentPage -= 1;
+			break;
+		case 'next':
+			currentPage += 1;
+			break;
+		case 'first':
+			currentPage = 1;
+			break;
+		case 'last':
+			currentPage = totalPages;
+			break;
+	}
+
+	updateFromTo(currentPage);
+	renderCurrentPage();
+
+	const paginatedProducts = await paginateProducts(orderedProducts, from, to);
+
+	renderProducts(paginatedProducts);
+
+	updatePaginationButtonsState(totalPages);
+};
+
+/**
+ * Handles the change event of the select element.
+ */
+const handleSelectChange = async () => {
+	const orderBy = document.querySelector('#order-by').value;
+
+	orderedProducts = orderProducts(productsArray, orderBy);
+	paginatedProducts = await paginateProducts(orderedProducts, from, to);
+
+	renderProducts(paginatedProducts);
+};
 
 /**
  * Products page event listener.
  */
 const productsEventListener = async () => {
-	//* Products variables
+	const orderSelect = document.querySelector('#order-by');
 
-	productsArray = await getProducts();
-	renderProducts(productsArray);
+	//* Products variables
+	productsArray = await getAllProducts();
+	totalPages = Math.ceil(productsArray.length / PRODUCTS_PER_PAGE);
+	orderedProducts = [...productsArray];
+	paginatedProducts = await paginateProducts(productsArray);
+	renderProducts(paginatedProducts);
 
 	//! event listeners
 
 	//* Search bar
 	document.querySelector('#search-button').addEventListener('click', searchBarListener);
+
+	//* Order select
+	orderSelect.addEventListener('change', handleSelectChange);
+
+	//* Pagination
+	document.querySelector('#previous-page').addEventListener('click', handlePaginationButtons);
+	document.querySelector('#next-page').addEventListener('click', handlePaginationButtons);
+	document.querySelector('#first-page').addEventListener('click', handlePaginationButtons);
+	document.querySelector('#last-page').addEventListener('click', handlePaginationButtons);
 };
 
 document.addEventListener('DOMContentLoaded', productsEventListener);
--- js/products/get-products.js
+++ js/products/get-products.js
@@ -1,17 +1,43 @@
 /**
- * Fetches a list of products
+ * Get a list of all products.
  *
- * @param {number} from  - The first product to get
- * @param {number} to  - The last product to get
- * @returns {Promise<Array>} - An array of products
+ * @returns {Promise<Array<{id, product_name, model_number, brand, price, img_url, product_type_id}>>} - An array of products
  */
-export const getProducts = async (from = 1, to = 20) => {
+export const getAllProducts = async () => {
 	const response = await fetch('../data/mock_data.json');
 	const products = await response.json();
 
 	await delay();
 
-	return products.slice(from - 1, to);
+	return products;
+};
+
+/**
+ * Get a list of products based on the given range.
+ *
+ * @param {Array<{id, product_name, model_number, brand, price, img_url, product_type_id}>} products - The list of products to paginate.
+ * @param {number} from - The first product to get.
+ * @param {number} to - The last product to get.
+ * @returns {Promise<Array<{id, product_name, model_number, brand, price, img_url, product_type_id}>>} - An array of products.
+ */
+export const paginateProducts = async (products, from = 1, to = 20) => {
+	if (!isValidRange(from, to, products)) return [];
+
+	const paginatedProducts = products.slice(from - 1, to);
+
+	await delay();
+
+	return new Promise((resolve) => resolve(paginatedProducts));
+};
+
+const isValidRange = (from, to, products) => {
+	if (from < 1 || to < 1) return false;
+	if (from > to) return false;
+	if (from > products.length) return false;
+	if (to > products.length) return false;
+	if (products.length === 0) return false;
+
+	return true;
 };
 
 const delay = (ms = 2000) => new Promise((resolve) => setTimeout(resolve, ms));
