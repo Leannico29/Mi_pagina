5f5119b423ff95e29b3b68cedda27aae87addaf0
Facundo Berges <facundo.h.berges@gmail.com>
2024-10-27 02:25:04 -0300

    Reestructuración login.js

--- js/login.js
+++ js/login.js
@@ -1,19 +1,28 @@
-document.addEventListener('DOMContentLoaded', function () {
+/**
+ * Event listeners to switch between login and register forms
+ */
+const loginEventListeners = () => {
 	const loginForm = document.getElementById('login-form');
 	const registerForm = document.getElementById('register-form');
 	const formTitle = document.getElementById('form-title');
 
-	document.getElementById('switch-to-register').addEventListener('click', function (event) {
+	document.getElementById('switch-to-register').addEventListener('click', (event) => {
 		event.preventDefault();
+
 		loginForm.style.display = 'none';
 		registerForm.style.display = 'block';
+
 		formTitle.textContent = 'Registro';
 	});
 
-	document.getElementById('switch-to-login').addEventListener('click', function (event) {
+	document.getElementById('switch-to-login').addEventListener('click', (event) => {
 		event.preventDefault();
+
 		registerForm.style.display = 'none';
 		loginForm.style.display = 'block';
+
 		formTitle.textContent = 'Iniciar sesión';
 	});
-});
+};
+
+document.addEventListener('DOMContentLoaded', loginEventListeners);
